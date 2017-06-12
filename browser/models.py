a77e508c1b278c3b266c8d77a7fc776bf4a6a9be
Lorenzo Gaborini <lorenzo.gaborini@unil.ch>
2017-06-12 13:56:53 +0200

    Added Price decimal field. Minor styling.

--- browser/models.py
+++ browser/models.py
@@ -10,9 +10,13 @@ class ProductDjangoItem(models.Model):
     productName = models.CharField(max_length=100)
     last_fetched = models.DateTimeField()
     productUrl = models.URLField()
+    productPrice = models.DecimalField(max_digits=5, decimal_places=2)
 
     def __str__(self):
         return self.productName
+
+    class Meta:
+        verbose_name = "ProductDjangoItem"
 
 
 class ProductReviewDjangoItem(models.Model):
@@ -28,3 +32,6 @@ class ProductReviewDjangoItem(models.Model):
     reviewTitle = models.CharField(max_length=100)
     reviewReply = models.CharField(max_length=10960)
     fit = models.CharField(max_length=100, null=True)
+
+    class Meta:
+        verbose_name = "ProductReviewDjangoItem"
